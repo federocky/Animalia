54648f78f306dc79bb5d9b658a6ffc265890bd3d
Federico Jácome <fede_rock_85@hotmail.com>
2021-01-28 20:33:38 +0100

    agregamos el slider, los componentes del tienda-main y y el filtro sin completarlo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header/header.component';
+import { NavBarComponent } from './components/shared/header/nav-bar/nav-bar.component';
+import { CarroComponent } from './components/shared/header/carro/carro.component';
+import { FooterComponent } from './components/shared/footer/footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavMovilComponent } from './components/shared/header/nav-movil/nav-movil.component';
+import { MenuUserComponent } from './components/shared/header/menu-user/menu-user.component';
+import { SliderComponent } from './components/shared/slider/slider.component';
+import { MainTiendaComponent } from './components/tienda/main-tienda/main-tienda.component';
+import { FiltrosTiendaComponent } from './components/tienda/filtros-tienda/filtros-tienda.component';
+import { ArticulosTiendaComponent } from './components/tienda/articulos-tienda/articulos-tienda.component';
+import { ArticuloCardTiendaComponent } from './components/tienda/articulo-card-tienda/articulo-card-tienda.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    CarroComponent,
+    FooterComponent,
+    NavMovilComponent,
+    MenuUserComponent,
+    SliderComponent,
+    MainTiendaComponent,
+    FiltrosTiendaComponent,
+    ArticulosTiendaComponent,
+    ArticuloCardTiendaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/tienda/articulos-tienda/articulos-tienda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-articulos-tienda',
+  templateUrl: './articulos-tienda.component.html',
+  styleUrls: ['./articulos-tienda.component.scss']
+})
+export class ArticulosTiendaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
